842a4c1c04d3ef7f1b870b671ed09f34b47c930d
Pavel Klochkov <paul.klochkov@gmail.com>
2019-06-16 10:36:41 +0200

    Prisma backend added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,22 @@
-// let's go!
+require("dotenv").config({ path: "./variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+// TODO: Use express middleware to handle cookies (JWT)
+
+// TODO: Use express middleware to populdate current user
+const server = createServer();
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(
+      `⚡️ Server is now running on http://localhost:${deets.port} 🚀`
+    );
+  }
+);
